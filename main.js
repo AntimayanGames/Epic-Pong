a760190b45c20c2875e5d1728518f7785c1f8d7f
Cosimos Cendo <cosimoscendo@gmail.com>
2018-12-28 02:19:37 -0500

    made game prettier, added ai difficulty settings, added win screen

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var rightScore = 0;
 var initialBallXvelocity = 12;
 const paddleEdgeBounceDampener = .002;
 var playerSpeed = 15;
+var AIspeed = 12;
 let rightPlayerY = Height / 2;
 let leftPlayerY = Height / 2;
 let rightPlayerWidth = 11;
@@ -17,6 +18,10 @@ let leftPlayerWidth = 11;
 let leftPlayerHeight = 100;
 var backgroundColor = 0;
 var AIerror = 4;
+var leftPlayerColor = '#FFAAAA';
+var rightPlayerColor = '#AAAAFF';
+var AIdifficulty = 2;
+var bool = true;
 
 var supertest;
 
@@ -28,6 +33,8 @@ function setup() {
   mgr.addScene(Timed);
   mgr.addScene(FreePlay);
   mgr.addScene(SinglePlayer);
+  mgr.addScene(rightWins);
+  mgr.addScene(leftWins);
   mgr.showNextScene();
   smash.pause();
   smash.currentTime = 0;
@@ -48,6 +55,8 @@ function Title() {
     strokeWeight(0);
     fill(255);
     textAlign(CENTER);
+    textSize(50);
+    text('AI Difficulty:', 1050, 240);
     textSize(100);
     text('Single Player', Width / 2, Height / 4 - 40);
     text('Normal', Width / 2, Height / 2 - 65);
@@ -60,13 +69,13 @@ function Title() {
       textSize(30);
       noStroke();
       fill(0, 100, 200);
-      text('Play against a friend', Width / 2, Height - 10);
+      text('Play against a friend (First to 7 points wins)', Width / 2, Height - 10);
     } else if (mouseX > 300 && mouseX < 900 && mouseY > rectX + 153 && mouseY < rectX + 153 + 100) { //timed
       rect(300, rectX + 153, 600, 100);
       textSize(30);
       noStroke();
       fill(0, 100, 200);
-      text('2 Player: Every 7 seconds a new ball will spawn', Width / 2, Height - 10);
+      text('2 Player: Every 7 seconds a new ball will spawn (First to 7 points wins)', Width / 2, Height - 10);
     } else if (mouseX > 300 && mouseX < 900 && mouseY > rectX + 2 * 153 && mouseY < rectX + 2 * 153 + 100) { //free play
       rect(300, rectX + 2 * 153, 600, 100);
       textSize(30);
@@ -78,22 +87,85 @@ function Title() {
       textSize(30);
       noStroke();
       fill(0, 100, 200);
-      text('Play by yourself against an AI', Width / 2, Height - 10);
+      text('Play by yourself against an AI (First to 7 points wins)', Width / 2, Height - 10);
+    }
+
+    if (AIdifficulty === 1) {
+      AIerror = 7;
+      AIspeed = 8;
+      textSize(50);
+      noStroke();
+      fill(255);
+      text('Easy', 1050, 350);
+    } else if (AIdifficulty === 2) {
+      AIerror = 5;
+      AIspeed = 10;
+      textSize(50);
+      fill(255);
+      noStroke();
+      text('Medium', 1050, 350);
+    } else if (AIdifficulty === 3) {
+      AIerror = 4;
+      AIspeed = 12;
+      textSize(50);
+      fill(255);
+      noStroke();
+      text('Hard', 1050, 350);
+    } else if (AIdifficulty === 4) {
+      AIerror = 2;
+      AIspeed = playerSpeed;
+      textSize(50);
+      fill(255);
+      noStroke();
+      text('Impossible', 1050, 350);
+    }
+    stroke(255);
+    noFill();
+    triangle(1050, 260, 1020, 300, 1080, 300);
+    triangle(1050, 410, 1020, 370, 1080, 370);
+
+    if (mouseX > 1000 && mouseX < 1100 && mouseY > 220 && mouseY < 320) {
+      fill(255, 255, 180);
+      stroke(255);
+      strokeWeight(4);
+      triangle(1050, 260, 1020, 300, 1080, 300);
+    } else if (mouseX > 1000 && mouseX < 1100 && mouseY > 330 && mouseY < 430) {
+      fill(255, 255, 180);
+      stroke(255);
+      strokeWeight(4);
+      triangle(1050, 410, 1020, 370, 1080, 370);
+    } else {
+      stroke(255);
+      noFill();
+      triangle(1050, 260, 1020, 300, 1080, 300);
+      triangle(1050, 410, 1020, 370, 1080, 370);
     }
   }
 
   this.mousePressed = function () {
     if (mouseX > 300 && mouseX < 900 && mouseY > rectX && mouseY < rectX + 100) {
+      clickAudio();
       mgr.showScene(Normal);
       setTimeout(ballSpawn, 1200);
     } else if (mouseX > 300 && mouseX < 900 && mouseY > rectX + 175 && mouseY < rectX + 175 + 100) {
+      clickAudio();
       mgr.showScene(Timed);
       setTimeout(ballSpawn, 1200);
-    } else if (mouseX > 300 && mouseX < 900 && mouseY > rectX + 2 * 175 && mouseY < rectX + 2 * 175 + 100) {
+    } else if (mouseX > 300 && mouseX < 900 && mouseY > rectX + 2 * 175 && mouseY < rectX + 2 * 175 + 100) { //free play
+      clickAudio();
       mgr.showScene(FreePlay);
-    } else if (mouseX > 250 && mouseX < 950 && mouseY > 50 && mouseY < 150) {
+    } else if (mouseX > 250 && mouseX < 950 && mouseY > 50 && mouseY < 150) { //single player
+      clickAudio();
       mgr.showScene(SinglePlayer);
       setTimeout(ballSpawn, 1200);
+    }
+
+    if (mouseX > 1000 && mouseX < 1100 && mouseY > 220 && mouseY < 320 && AIdifficulty > 1) {
+      clickAudio();
+      AIdifficulty -= 1;
+    } else if (mouseX > 1000 && mouseX < 1100 && mouseY > 330 && mouseY < 430 && AIdifficulty < 4) {
+      clickAudio();
+      AIdifficulty += 1;
     }
   }
 }
@@ -103,14 +175,16 @@ function Normal() {
   this.draw = function () {
     background(backgroundColor);
     strokeWeight(0);
-    fill(255);
+    fill(rightPlayerColor);
     rect(Width - 50 - rightPlayerWidth, rightPlayerY, rightPlayerWidth, rightPlayerHeight); //right player
+    fill(leftPlayerColor);
     rect(50, leftPlayerY, leftPlayerWidth, leftPlayerHeight); //left player
     move();
     textSize(100);
     textAlign(CENTER);
-    text(leftScore, Width / 4, Height / 4);
-    text(rightScore, 3 * Width / 4, Height / 4);
+    text(leftScore, Width / 2 - 100, Height / 4);
+    fill(rightPlayerColor);
+    text(rightScore, Width / 2 + 100, Height / 4);
     textSize(20);
 
     for (i = 0; i <= balls.length - 1; i++) {
@@ -132,6 +206,10 @@ function Normal() {
       }
     }
     textSize(30);
+    fill(255);
+    strokeWeight(9);
+    line(Width / 2, 0, Width / 2, Height);
+    strokeWeight(0);
     text('Main Menu', 1120, 25);
   }
 
@@ -157,13 +235,16 @@ function Timed() {
   this.draw = function () {
     background(backgroundColor);
     fill(255);
+    fill(rightPlayerColor);
     rect(Width - 50 - rightPlayerWidth, rightPlayerY, rightPlayerWidth, rightPlayerHeight); //right player
+    fill(leftPlayerColor);
     rect(50, leftPlayerY, leftPlayerWidth, leftPlayerHeight); //left player
     move();
     textSize(100);
     textAlign(CENTER);
-    text(leftScore, Width / 4, Height / 4);
-    text(rightScore, 3 * Width / 4, Height / 4);
+    text(leftScore, Width / 2 - 100, Height / 4);
+    fill(rightPlayerColor);
+    text(rightScore, Width / 2 + 100, Height / 4);
     for (i = 0; i <= balls.length - 1; i++) {
       balls[i].show();
       balls[i].move();
@@ -181,6 +262,10 @@ function Timed() {
       }
     }
     textSize(30);
+    fill(255);
+    strokeWeight(9);
+    line(Width / 2, 0, Width / 2, Height);
+    strokeWeight(0);
     text('Main Menu', 1120, 25);
   }
   this.mousePressed = function () {
@@ -207,16 +292,19 @@ function SinglePlayer() {
     this.randoNum = 0;
   }
   this.draw = function () {
+    fill(255);
     background(backgroundColor);
     strokeWeight(0);
-    fill(255);
+    fill(rightPlayerColor);
     rect(Width - 50 - rightPlayerWidth, rightPlayerY, rightPlayerWidth, rightPlayerHeight); //right player
+    fill(leftPlayerColor);
     rect(50, leftPlayerY, leftPlayerWidth, leftPlayerHeight); //left player
     move();
     textSize(100);
     textAlign(CENTER);
-    text(leftScore, Width / 4, Height / 4);
-    text(rightScore, 3 * Width / 4, Height / 4);
+    text(leftScore, Width / 2 - 100, Height / 4);
+    fill(rightPlayerColor);
+    text(rightScore, Width / 2 + 100, Height / 4);
     textSize(20);
     this.AI();
     this.randomNum();
@@ -225,21 +313,37 @@ function SinglePlayer() {
       balls[i].move();
     }
     for (let i = balls.length - 1; i >= 0; i--) { //score
-      if (balls[i].offScreenRight()) {
+      if (balls[i].offScreenRight() && leftScore < 6) {
         balls.splice(i, 1);
         leftScore += 1;
         fortniteAudio();
         this.randoNum = 0;
         this.d = setTimeout(ballSpawn, 1200);
-      } else if (balls[i].offScreenLeft()) {
+      } else if (balls[i].offScreenLeft() && rightScore < 6) {
         balls.splice(i, 1);
         rightScore += 1;
         fortniteAudio();
         this.randoNum = 0;
         this.d = setTimeout(ballSpawn, 1200);
+      } else if (balls[i].offScreenRight() && leftScore == 6) {
+        balls.splice(i, 1);
+        leftScore += 1;
+        fortniteAudio();
+        this.randoNum = 0;
+        mgr.showScene(leftWins);
+      } else if (balls[i].offScreenLeft() && rightScore == 6) {
+        balls.splice(i, 1);
+        rightScore += 1;
+        fortniteAudio();
+        this.randoNum = 0;
+        mgr.showScene(rightWins);
       }
     }
     textSize(30);
+    fill(255);
+    strokeWeight(9);
+    line(Width / 2, 0, Width / 2, Height);
+    strokeWeight(0);
     text('Main Menu', 1120, 25);
   }
 
@@ -258,9 +362,9 @@ function SinglePlayer() {
   this.AI = function () {
     for (i = 0; i < balls.length; i++) {
       if (balls[i].y > rightPlayerY + 3 * rightPlayerHeight / 4 + this.randoNum && balls[i].x > Width / 2) {
-        rightPlayerY += playerSpeed - 3;
+        rightPlayerY += AIspeed;
       } else if (balls[i].y < rightPlayerY + rightPlayerHeight / 4 + this.randoNum && balls[i].x > Width / 2) {
-        rightPlayerY -= playerSpeed - 3;
+        rightPlayerY -= AIspeed;
       } else {
         rightPlayerY += 0;
       }
@@ -272,6 +376,95 @@ function SinglePlayer() {
       if (balls[i].x < 100 && balls[i].x > 50) {
         this.randoNum = floor(Math.pow(random(-AIerror, AIerror), 3));
       }
+    }
+  }
+}
+
+function rightWins() {
+  this.draw = function () {
+    background(0);
+    textSize(100);
+    textAlign(CENTER);
+    fill(leftPlayerColor);
+    text(leftScore, Width / 2 - 100, Height / 4);
+    fill(rightPlayerColor);
+    text(rightScore, Width / 2 + 100, Height / 4);
+    fill(255);
+    noStroke();
+    text('Player 2 Wins!', Width / 2, Height / 2);
+    textSize(60);
+    text('Play Again', Width / 2, 500);
+    text('Main Menu', Width / 2, 600);
+    strokeWeight(1.5);
+    stroke(255);
+    if (mouseX > 440 && mouseX < 760 && mouseY > 445 && mouseY < 515) {
+      noFill();
+      rect(440, 445, 320, 70);
+    } else if (mouseX > 440 && mouseX < 760 && mouseY > 545 && mouseY < 615) {
+      noFill();
+      rect(440, 545, 320, 70);
+    }
+  }
+  this.mousePressed = function () {
+    if (mouseX > 440 && mouseX < 760 && mouseY > 445 && mouseY < 515) {
+      mgr.showScene(SinglePlayer);
+      leftScore = 0;
+      rightScore = 0;
+      rightPlayerY = Height / 2;
+      leftPlayerY = Height / 2;
+      balls = [];
+      setTimeout(ballSpawn, 1200);
+    } else if (mouseX > 440 && mouseX < 760 && mouseY > 545 && mouseY < 615) {
+      mgr.showScene(Title);
+      leftScore = 0;
+      rightScore = 0;
+      rightPlayerY = Height / 2;
+      leftPlayerY = Height / 2;
+      balls = [];
+      clearTimeout(this.d);
+    }
+  }
+}
+
+function leftWins() {
+  this.draw = function () {
+    background(0);
+    textSize(100);
+    textAlign(CENTER);
+    fill(leftPlayerColor);
+    text(leftScore, Width / 2 - 100, Height / 4);
+    fill(rightPlayerColor);
+    text(rightScore, Width / 2 + 100, Height / 4);
+    fill(255);
+    noStroke();
+    text('Player 1 Wins!', Width / 2, Height / 2);
+    textSize(60);
+    text('Play Again', Width / 2, 500);
+    text('Main Menu', Width / 2, 600);
+    strokeWeight(1.5);
+    stroke(255);
+    if (mouseX > 440 && mouseX < 760 && mouseY > 445 && mouseY < 515) {
+      noFill();
+      rect(440, 445, 320, 70);
+    } else if (mouseX > 440 && mouseX < 760 && mouseY > 545 && mouseY < 615) {
+      noFill();
+      rect(440, 545, 320, 70);
+    }
+  }
+  this.mousePressed = function () {
+    if (mouseX > 440 && mouseX < 760 && mouseY > 445 && mouseY < 515) {
+      mgr.showScene(SinglePlayer);
+      leftScore = 0;
+      rightScore = 0;
+      rightPlayerY = Height / 2;
+      leftPlayerY = Height / 2;
+      setTimeout(ballSpawn, 1200);
+    } else if (mouseX > 440 && mouseX < 760 && mouseY > 545 && mouseY < 615) {
+      mgr.showScene(Title);
+      leftScore = 0;
+      rightScore = 0;
+      rightPlayerY = Height / 2;
+      leftPlayerY = Height / 2;
     }
   }
 }
@@ -315,8 +508,8 @@ class Ball {
     this.terminalYvelocity = 15;
   }
   show() {
-    fill(255);
-    noStroke();
+    fill(255, 255, 180);
+    //noStroke();
     ellipse(this.x, this.y, ballRadius);
   }
 
